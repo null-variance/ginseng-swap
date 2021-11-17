dae7bf7435049bf90904f4eb5ed6d75c655db952
LucaSilverhand <levinswap0@gmail.com>
2021-11-17 23:18:30 +0400

    tests

--- test/test.js
+++ test/test.js
@@ -1,19 +1,22 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
+describe("Ginseng", function () {
   it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+    const GinsengSwap = await ethers.getContractFactory("Ginseng");
+    const ginsengswap = await GinsengSwap.deploy("Hello, world!");
+    await ginsengswap.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    // expect(await ginsengswap.greet()).to.equal("Hello, world!");
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    const setFeeSetter = await ginsengswap.setFeeSetter(/*"multisig deployment address"*/);
+    const setFee = await ginsengswap.setFee(/*"fee amount"*/);
 
     // wait until the transaction is mined
-    await setGreetingTx.wait();
+    await setFeeSetter.wait();
+    await setFee.wait();
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    expect(await ginsengswap.setFeeSetter()).to.equal(/*"multisig deployment address"*/);
+    expect(await ginsengswap.setFee()).to.equal(/*"fee amount"*/);
   });
 });
